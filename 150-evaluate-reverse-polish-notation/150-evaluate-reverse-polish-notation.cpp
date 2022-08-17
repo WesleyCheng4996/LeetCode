c424ff3a15ad6668592dcb4f88901a90c5865258
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-08-17 21:40:18 +0800

    Time: 13 ms (74.04%), Space: 11.9 MB (89.34%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -10,37 +10,34 @@ public:
         reverse(tokens.begin(), tokens.end());
         
         constexpr void* labels[] = {&&ISOPERATOR, &&ISDIGIT, &&DONE};
-        
+        constexpr void* operators[] = {&&MUL, &&ADD, &&DONE, &&SUB, &&DONE, &&DIV};
         while(!tokens.empty()){
             int a, b;
             goto *labels[isNumber(tokens.back())];
         ISDIGIT:
             a = stoi(tokens.back());
-            stack.push_back(a);
             goto DONE;
         ISOPERATOR:
             b = stack.back();
             stack.pop_back();
             a = stack.back();
             stack.pop_back();
-            switch(tokens.back().back()){
-                case '+':
-                    a += b;
-                    break;
-                case '-':
-                    a -= b;
-                    break;
-                case '*':
-                    a *= b;
-                    break;
-                case '/':
-                    a /= b;
-                    break;
-            }
-            stack.push_back(a);
-        DONE:
-            tokens.pop_back();
+            goto *operators[tokens.back().back() - 42];
+        ADD:
+            a += b;
+            goto DONE;
+        SUB:
+            a -=b ;
+            goto DONE;
+        MUL:
+            a *= b;
+            goto DONE;
+        DIV: a /= b;
+            goto DONE;
 
+        DONE:
+            stack.push_back(a);
+            tokens.pop_back();
         }
         
         return stack.back();
