b6a3926fe5440416084a0b2eb7a40bebc187fe24
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-08-17 21:29:11 +0800

    Time: 21 ms (33.88%), Space: 11.8 MB (98.42%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        
+        if(tokens.size() == 1){
+            return stoi(tokens.back());
+        }
+        
+        vector<int>stack;
+        reverse(tokens.begin(), tokens.end());
+        
+        stack.push_back(stoi(tokens.back()));
+        tokens.pop_back();
+        stack.push_back(stoi(tokens.back()));
+        tokens.pop_back();
+        
+        constexpr void* labels[] = {&&ISOPERATOR, &&ISDIGIT, &&DONE};
+        
+        while(!tokens.empty()){
+            int a, b;
+            goto *labels[isNumber(tokens.back())];
+        ISDIGIT:
+            stack.push_back(stoi(tokens.back()));
+            goto DONE;
+        ISOPERATOR:
+            b = stack.back();
+            stack.pop_back();
+            a = stack.back();
+            stack.pop_back();
+            switch(tokens.back().back()){
+                case '+':
+                    a += b;
+                    break;
+                case '-':
+                    a -= b;
+                    break;
+                case '*':
+                    a *= b;
+                    break;
+                case '/':
+                    a /= b;
+                    break;
+            }
+            stack.push_back(a);
+        DONE:
+            tokens.pop_back();
+
+        }
+        
+        return stack.back();
+    }
+                            
+    bool isNumber(const string& str){
+        return str.size() > 1 || (str.size() == 1 && isdigit(str.back())) ;
+    }
+};
