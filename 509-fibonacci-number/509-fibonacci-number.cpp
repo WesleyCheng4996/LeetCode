1038a38c28dbdcf084edf1a4bd5d9a71ca58315f
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-08-25 17:42:44 +0800

    Time: 0 ms (100.00%), Space: 6 MB (39.80%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
-    int fib(int n) {
-        return n < 2 ? n : fib(n - 1) + fib(n - 2);
+    void Multiply(int arr[4], int brr[4]) {
+    int ans[4] = { arr[0] * brr[0] + arr[1] * brr[2],
+                    arr[0] * brr[1] + arr[1] * brr[3],
+                    arr[2] * brr[0] + arr[3] * brr[2],
+                    arr[2] * brr[1] + arr[3] * brr[3]
+    };
+    arr[0] = ans[0];
+    arr[1] = ans[1];
+    arr[2] = ans[2];
+    arr[3] = ans[3];
+    
+}
+
+int fib(int n) {
+
+    if (n < 2) {
+        return n;
     }
+
+    int arr[4] = { 1, 1, 1, 0 };
+    int base[4] = { 1, 1, 1, 0 };
+    n -= 2;
+    for (;;) {
+        if (n & 1) {
+            Multiply(arr, base);
+        }
+        n >>= 1;
+        if (n <= 0) {
+            break;
+        }
+        Multiply(base, base);
+    }
+
+    return arr[0];
+    }
+
 };
