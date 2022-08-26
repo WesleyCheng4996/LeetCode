ef6afd59780800c024d6f48bdcb284f70ea58398
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-08-26 20:24:36 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (39.80%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,39 +1,19 @@
+template<size_t N>
+struct Fib {
+	constexpr Fib() : arr() {
+		arr[0] = 0;
+		arr[1] = 1;
+		for (int i = 2; i < N; ++i) {
+			arr[i] = arr[i - 1] + arr[i - 2];
+		}
+	}
+	int arr[N];
+};
+constexpr Fib<31> f;
 class Solution {
 public:
-    void Multiply(int arr[4], int brr[4]) {
-        int ans[4] = { arr[0] * brr[0] + arr[1] * brr[2],
-                        arr[0] * brr[1] + arr[1] * brr[3],
-                        arr[2] * brr[0] + arr[3] * brr[2],
-                        arr[2] * brr[1] + arr[3] * brr[3]
-        };
-        arr[0] = ans[0];
-        arr[1] = ans[1];
-        arr[2] = ans[2];
-        arr[3] = ans[3];
-
-    }
-
     int fib(int n) {
-
-        if (n < 2) {
-            return n;
-        }
-
-        int arr[4] = { 1, 1, 1, 0 };
-        int base[4] = { 1, 1, 1, 0 };
-        n -= 2;
-        for (;;) {
-            if (n & 1) {
-                Multiply(arr, base);
-            }
-            n >>= 1;
-            Multiply(base, base);
-            if (n <= 0) {
-                break;
-            }
-        }
-
-        return arr[0];
+        return f.arr[n];
     }
 
 };
