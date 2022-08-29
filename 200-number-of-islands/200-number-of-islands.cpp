3a3d6fcdb6295463adb051fc205381fc71b410e8
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-08-29 23:54:30 +0800

    Time: 34 ms (96.55%), Space: 12.2 MB (83.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,47 +1,28 @@
-void doNothing(vector<vector<char>>& grid, int m, int n);
-
-void destroyIsland(vector<vector<char>>& grid, int m, int n);
-
-void (*funcs[])(vector<vector<char>>&, int, int) = { &doNothing, &destroyIsland };
-
-void destroyIsland(vector<vector<char>>& grid, int m, int n){
-    const int mm = grid.size(), nn = grid[0].size();
-    
-    --grid[m][n];
-    
-    if(m > 0) {
-        funcs[grid[m - 1][n] - '0'](grid, m - 1, n);
-    }
-    if(m + 1 < mm) {
-        funcs[grid[m + 1][n] - '0'](grid, m + 1, n);
-    }
-    if(n > 0) {
-        funcs[grid[m][n - 1] - '0'](grid, m, n - 1);
-    }
-    if(n + 1 < nn) {
-        funcs[grid[m][n + 1] - '0'](grid, m, n + 1);
-    }
-    return;
-}
-
-void doNothing(vector<vector<char>>& grid, int m, int n) {
-    return;
-}
-
-
 class Solution {
 public:
+    
+    void turn_to_dust(vector<vector<char>>& grid, int i, int j, int m, int n){
+        if (i<0 || j<0 || i==m || j==n || grid[i][j]=='0') return;
+        grid[i][j]='0';
+        turn_to_dust(grid,i+1,j,m,n);
+        turn_to_dust(grid,i,j+1,m,n);
+        turn_to_dust(grid,i-1,j,m,n);
+        turn_to_dust(grid,i,j-1,m,n);
+        return;
+    }
+    
     int numIslands(vector<vector<char>>& grid) {
-        const int m = grid.size(), n = grid[0].size();
-        int count = 0;
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) { 
-                if(grid[i][j] == '1'){
-                    destroyIsland(grid, i, j);
-                    ++count;
+        int m = grid.size();
+        int n = grid[0].size();
+        int res=0;
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (grid[i][j]=='1'){
+                    res++;
+                    turn_to_dust(grid,i,j,m,n);
                 }
             }
         }
-        return count;
+        return res;
     }
 };
