fb99bb1cf43dee8e57f36f9228b6429ba6aab9c6
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-08-30 01:00:03 +0800

    Time: 893 ms (51.20%), Space: 81.7 MB (62.02%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -4,24 +4,20 @@ struct Constant
     constexpr Constant() :
         arr()
     {
-        int count = 0;
         arr[0] = 0;
         arr[1] = 0;
-        for (int i = 1; i < N; ++i) {
-            count += (i << 3);
-            arr[i + 1] = count;
+        for (int i = 2; i < N; ++i) {
+            arr[i] = ((i * (i - 1)) << 2);
         }
     }
-    int arr[N + 1];
+    int arr[N];
 };
-
-
-Constant<30> c;
 
 
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
+        constexpr Constant<30> c;
         unordered_map<int, int> mp;
         int nums_len = nums.size(), counter = 0;
         for (int i = 0; i < nums_len; ++i) {
