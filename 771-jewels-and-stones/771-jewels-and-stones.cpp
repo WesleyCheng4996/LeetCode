95f3f01bb76b5923d93daebc483959e7a52a0b42
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-08-31 17:30:31 +0800

    Time: 7 ms (17.19%), Space: 6 MB (95.44%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int ans = 0;
-        auto begin = stones.begin(), end = stones.end();
+        string::iterator begin = stones.begin(), end = stones.end();
         for(char ch: jewels) {
             ans += count(begin, end, ch);
         }
