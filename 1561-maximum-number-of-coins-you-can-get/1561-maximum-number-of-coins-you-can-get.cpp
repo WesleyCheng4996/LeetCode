f9efe5659511d7b8c77dc7f8580e1bb22362bea0
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-08-31 18:01:35 +0800

    Time: 212 ms (37.15%), Space: 53.4 MB (91.88%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -3,7 +3,7 @@ public:
     int maxCoins(vector<int>& piles) {
         int can_pick_times = piles.size() / 3, ans = 0;
         sort(piles.begin(), piles.end());
-        for(int i = can_pick_times; can_pick_times--; (++i)++) {
+        for(int i = can_pick_times; can_pick_times--; i += 2) {
             ans += piles[i];
         }
         return ans;
