8aee56ce857af53d591e02d617d69d7190992aba
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-08-31 17:47:23 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (30.54%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        
-        reverse(word.begin(), ++word.begin() + word.find(ch));
+        auto begin = word.begin();
+        reverse(begin, ++begin + word.find(ch));
         return word;
     }
 };
