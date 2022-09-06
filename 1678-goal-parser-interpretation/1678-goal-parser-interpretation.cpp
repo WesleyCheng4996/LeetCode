14e1399e381d548fa2eb6d19b5e905677b16bc27
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-06 17:41:42 +0800

    Time: 16 ms (7.80%), Space: 7.9 MB (6.74%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string interpret(string command) {
+        command = regex_replace(command, regex("\\(\\)"), "o");
+        command = regex_replace(command, regex("\\(al\\)"), "al");
+
+        return command;
+    }
+};
