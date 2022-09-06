718ffcc6963a9e6f68e5f13786b9dbcf6d72c4d8
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-06 16:00:58 +0800

    Time: 186 ms (37.38%), Space: 26.8 MB (60.83%) - LeetHub

---
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -0,0 +1,78 @@
+
+class Solution {
+public:
+    int longestBeautifulSubstring(string word) {
+        int curr_len = 0, max_len = 0;
+        char check = 0;
+        for(int i = 0, end = word.size(); i < end; ++i) {
+            switch(word[i]) {
+                case 'a':
+                    if((check & 0b01111) == 0) {
+                        check |= 0b10000;
+                        ++curr_len;
+                    } else {
+                        if(curr_len > max_len && check == 0b11111) {
+                            max_len = curr_len;
+                        }
+                        curr_len = 1;
+                        check = 0b10000;
+                    }
+                    break;
+                case 'e':
+                    if((check & 0b10000) == 0b10000 && (check & 0b00111) == 0) {
+                        check |= 0b01000;
+                        ++curr_len;
+                    } else {
+                        if(curr_len > max_len && check == 0b11111) {
+                            max_len = curr_len;
+                        }
+                        curr_len = 0;
+                        check = 0;
+                    }
+                    break;
+                case 'i':
+                    if((check & 0b11000) == 0b11000 && (check & 0b00011) == 0) {
+                        check |= 0b00100;
+                        ++curr_len;
+                    } else {
+                        if(curr_len > max_len && check == 0b11111) {
+                            max_len = curr_len;
+                        }
+                        curr_len = 0;
+                        check = 0;
+                    }
+                    break;
+                case 'o':
+                    if((check & 0b11100) == 0b11100 && (check & 0b00001) == 0) {
+                        check |= 0b00010;
+                        ++curr_len;
+                    } else {
+                        if(curr_len > max_len && check == 0b11111) {
+                            max_len = curr_len;
+                        }
+                        curr_len = 0;
+                        check = 0;
+                    }
+                    break;
+                case 'u':
+                    if((check & 0b11110) == 0b11110) {
+                        check |= 0b00001;
+                        ++curr_len;
+                    } else {
+                        if(curr_len > max_len && check == 0b11111) {
+                            max_len = curr_len;
+                        }
+                        curr_len = 0;
+                        check = 0;
+                    }
+                    break;
+                default:
+                    ++curr_len;
+            }
+        }
+        if(curr_len > max_len && check == 0b11111) {
+            max_len = curr_len;
+        }
+        return max_len;
+    }
+};
