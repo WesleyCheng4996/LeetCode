c16da2573b98086c480e1f34ca3de92b88b17486
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-06 18:49:10 +0800

    Time: 309 ms (65.89%), Space: 123.8 MB (94.49%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -4,9 +4,8 @@ public:
         vector<int> ans(nums.size());
         int sign[] = { 1, 0 };
         for(int i = 0, end = nums.size(); i < end; ++i) {
-            int &ref = sign[nums[i] > 0];
-            ans[ref] = nums[i];
-            ref += 2;
+            ans[sign[nums[i] > 0]] = nums[i];
+            sign[nums[i] > 0] += 2;
         }
         return ans;
     }
