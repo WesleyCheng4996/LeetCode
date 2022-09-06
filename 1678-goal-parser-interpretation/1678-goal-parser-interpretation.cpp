ceceb3f8773d526401bf4648572b5510047b132b
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-06 17:42:26 +0800

    Time: 11 ms (7.80%), Space: 7.9 MB (6.74%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     string interpret(string command) {
-        command = regex_replace(command, regex("\\(\\)"), "o");
-        command = regex_replace(command, regex("\\(al\\)"), "al");
-
-        return command;
+        return regex_replace(regex_replace(command, regex("\\(al\\)"), "al"), regex("\\(\\)"), "o");;
     }
 };
