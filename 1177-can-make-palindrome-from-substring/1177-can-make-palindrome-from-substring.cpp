29997be6474779128693d134582bb0a660e76dbf
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-06 21:55:52 +0800

    Time: 660 ms (66.09%), Space: 95 MB (88.37%) - LeetHub

--- 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
-        vector<int*> mp(s.size() + 1);
-        for (auto& x : mp) {
-            x = new int[26]();
-        }
-        vector<bool> ans;
-        ans.reserve(queries.size());
-        for (char x : s) {
-            ++mp[0][x - 'a'];
-        }
-        for (int i = 1, end = s.size(); i < end; ++i) {
-            memcpy(mp[i], mp[i - 1], sizeof(int) * 26);
-            --mp[i][s[i - 1] - 'a'];
-        }
-        for (vector<int> vec : queries) {
-            ++vec[1];
-            int k = 0;
-            for (int i = 0; i < 26; ++i) {
-                k += (mp[vec[0]][i] - mp[vec[1]][i]) & 1;
-            }
-            ans.push_back((k >> 1) <= vec[2]);
-        }
-        return ans;
+    int mask = 0;
+    vector<int> ps(1);
+    for (char c : s)
+        ps.push_back(mask ^= 1 << (c - 'a'));
+
+    vector<bool> r;
+    for (auto &q : queries) {
+        int odds = __builtin_popcount(ps[q[1] + 1] ^ ps[q[0]]);
+        r.push_back(q[2] >= odds / 2);
     }
+    return r;
+}
 };
