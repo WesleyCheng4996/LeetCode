f5cc6347af43412cae396711bdedcf7c310ebd36
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-06 18:25:58 +0800

    Time: 26 ms (90.59%), Space: 12.7 MB (89.88%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -3,9 +3,8 @@ public:
     int minimumLength(string s) {
         string::iterator b = s.begin(), e = s.end() - 1;
         while (b < e && *b == *e) {
-            char temp(*b);
-            while (b < e && *(++b) == temp);
-            while (b < e && *(--e) == temp);
+            while (b < e && *(++b) == *e);
+            while (b < e && *(--e) == *(b - 1));
         };
         return e - b > 0 ? e - b + 1 : *(b - 1) != *b;
     }
