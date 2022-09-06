039211dad2d4d05e11df3f99b57759ce326ca9b4
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-06 16:12:36 +0800

    Time: 131 ms (70.58%), Space: 26.9 MB (60.83%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -2,73 +2,72 @@
 class Solution {
 public:
     int longestBeautifulSubstring(string word) {
+        void* labels[26];
+        labels['a' - 'a'] = &&_label_A;
+        labels['e' - 'a'] = &&_label_E;
+        labels['i' - 'a'] = &&_label_I;
+        labels['o' - 'a'] = &&_label_O;
+        labels['u' - 'a'] = &&_label_U;
         int curr_len = 0, max_len = 0;
         char check = 0;
+        
         for(int i = 0, end = word.size(); i < end; ++i) {
-            switch(word[i]) {
-                case 'a':
-                    if((check & 0b01111) == 0) {
-                        check |= 0b10000;
-                        ++curr_len;
-                    } else {
-                        if(curr_len > max_len && check == 0b11111) {
-                            max_len = curr_len;
-                        }
-                        curr_len = 1;
-                        check = 0b10000;
-                    }
-                    break;
-                case 'e':
-                    if((check & 0b10111) == 0b10000) {
-                        check |= 0b01000;
-                        ++curr_len;
-                    } else {
-                        if(curr_len > max_len && check == 0b11111) {
-                            max_len = curr_len;
-                        }
-                        curr_len = 0;
-                        check = 0;
-                    }
-                    break;
-                case 'i':
-                    if((check & 0b11011) == 0b11000) {
-                        check |= 0b00100;
-                        ++curr_len;
-                    } else {
-                        if(curr_len > max_len && check == 0b11111) {
-                            max_len = curr_len;
-                        }
-                        curr_len = 0;
-                        check = 0;
-                    }
-                    break;
-                case 'o':
-                    if((check & 0b11101) == 0b11100) {
-                        check |= 0b00010;
-                        ++curr_len;
-                    } else {
-                        if(curr_len > max_len && check == 0b11111) {
-                            max_len = curr_len;
-                        }
-                        curr_len = 0;
-                        check = 0;
-                    }
-                    break;
-                case 'u':
-                    if((check & 0b11110) == 0b11110) {
-                        check |= 0b00001;
-                        ++curr_len;
-                    } else {
-                        if(curr_len > max_len && check == 0b11111) {
-                            max_len = curr_len;
-                        }
-                        curr_len = 0;
-                        check = 0;
-                    }
-                    break;
-                default:
-                    ++curr_len;
-            }
+            goto *labels[word[i] - 'a'];
+        _label_A:
+            if((check & 0b01111) == 0) {
+                check |= 0b10000;
+                ++curr_len;
+            } else {
+                if(curr_len > max_len && check == 0b11111) {
+                    max_len = curr_len;
+                }
+                curr_len = 1;
+                check = 0b10000;
+            } continue;
+        _label_E:
+            if((check & 0b10111) == 0b10000) {
+                check |= 0b01000;
+                ++curr_len;
+            } else {
+                if(curr_len > max_len && check == 0b11111) {
+                    max_len = curr_len;
+                }
+                curr_len = 0;
+                check = 0;
+            } continue;
+        _label_I:
+            if((check & 0b11011) == 0b11000) {
+                check |= 0b00100;
+                ++curr_len;
+            } else {
+                if(curr_len > max_len && check == 0b11111) {
+                    max_len = curr_len;
+                }
+                curr_len = 0;
+                check = 0;
+            } continue;
+        _label_O:
+            if((check & 0b11101) == 0b11100) {
+                check |= 0b00010;
+                ++curr_len;
+            } else {
+                if(curr_len > max_len && check == 0b11111) {
+                    max_len = curr_len;
+                }
+                curr_len = 0;
+                check = 0;
+            } continue;
+        _label_U: 
+            if((check & 0b11110) == 0b11110) {
+                check |= 0b00001;
+                ++curr_len;
+            } else {
+                if(curr_len > max_len && check == 0b11111) {
+                    max_len = curr_len;
+                }
+                curr_len = 0;
+                check = 0;
+            } continue;
         }
         if(curr_len > max_len && check == 0b11111) {
             max_len = curr_len;
