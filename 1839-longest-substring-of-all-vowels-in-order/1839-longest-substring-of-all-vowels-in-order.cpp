6a0bfbcee41be291637d0791b83e211b0c16f6e1
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-06 16:20:04 +0800

    Time: 115 ms (81.11%), Space: 26.8 MB (89.26%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -23,37 +23,37 @@ public:
             goto *labels[word[i] - 'a'];
         _label_A:
             if((check & 0b01111) == 0) {
-                check |= 0b10000;
+                check = 0b10000;
                 ++curr_len;
             } else {
                 func();
                 ++curr_len;
-                check |= 0b10000;
+                check = 0b10000;
             } continue;
         _label_E:
             if((check & 0b10111) == 0b10000) {
-                check |= 0b01000;
+                check = 0b11000;
                 ++curr_len;
             } else {
                 func();
             } continue;
         _label_I:
             if((check & 0b11011) == 0b11000) {
-                check |= 0b00100;
+                check = 0b11100;
                 ++curr_len;
             } else {
                 func();
             } continue;
         _label_O:
             if((check & 0b11101) == 0b11100) {
-                check |= 0b00010;
+                check = 0b11110;
                 ++curr_len;
             } else {
                 func();
             } continue;
         _label_U: 
             if((check & 0b11110) == 0b11110) {
-                check |= 0b00001;
+                check = 0b11111;
                 ++curr_len;
             } else {
                 func();
