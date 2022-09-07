25b335ba8abca3b770783079ea4fa85df24b9164
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-07 15:24:12 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (76.25%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,10 +1,25 @@
+template<int N>
+struct bit_num {
+    constexpr bit_num() :
+        arr() 
+    {
+        for(int i = 0; i < N; ++i) {
+            arr[i] = 1 << i;
+        }
+    }
+    
+    int arr[N];
+};
+
+constexpr bit_num<32> b;
+
 class Solution {
 public:
     int bitwiseComplement(int n) {
         int x = ~n;
         for(int i = 31; i >= 0; --i) {
-            if(x & (1 << i)) {
-                x &= ~(1 << i);
+            if(x & b.arr[i]) {
+                x &= ~(b.arr[i]);
             }
             else {
                 return x;
