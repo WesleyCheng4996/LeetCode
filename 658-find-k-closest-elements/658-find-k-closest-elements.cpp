6a280aba88c61456136606ab053d76f41e76070b
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-08 16:39:21 +0800

    Time: 109 ms (48.62%), Space: 31.1 MB (94.14%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int>::iterator b = arr.begin(), e = arr.end();
+        vector<int>::iterator first_it = b;
+        for(vector<int>::iterator it(b); it < e; ++it) {
+            if(abs(*it - x) < abs(*first_it - x)) {
+                first_it = it;
+            }
+        }
+        vector<int>::iterator last_it = first_it + 1;
+        while(last_it - first_it < k) {
+            if(first_it == b) {
+                ++last_it;
+            } else if(last_it == e) {
+                --first_it;
+            } else if(abs(*(first_it - 1) - x) <= abs(*(last_it) - x)) {
+                --first_it;
+            } else if(abs(*(first_it - 1) - x) > abs(*(last_it) - x)) {
+                ++last_it;
+            } 
+        }
+        return vector<int>(first_it, last_it);
+    }
+};
