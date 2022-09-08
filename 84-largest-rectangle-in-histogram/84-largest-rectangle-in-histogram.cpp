cf8fd5f50bb057eb347af6e762499cbf46180876
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-08 17:58:06 +0800

    Time: 309 ms (43.13%), Space: 89.1 MB (21.34%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -3,42 +3,40 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         int size = heights.size(), ans = 0;
         vector<int>::iterator b = heights.begin(), e = heights.end();
-        vector<pair<vector<int>::iterator, vector<int>::iterator>>firstit_lastit(size);
-        firstit_lastit[0].first = b;
-        firstit_lastit[size - 1].second = e;
+        vector<vector<int>::iterator>lastit(size);
+        lastit[size - 1] = e;
         stack<pair<int, int>>idx_val;
         idx_val.push({ 0, heights[0] });
         for (int i = 1; i < size; ++i) {
             while (!idx_val.empty() && idx_val.top().second > heights[i]) {
-                firstit_lastit[idx_val.top().first].second = b + i;
+                lastit[idx_val.top().first] = b + i;
                 idx_val.pop();
             }
             idx_val.push({ i, heights[i] });
         }
         while (!idx_val.empty()) {
-            firstit_lastit[idx_val.top().first].second = e;
+            lastit[idx_val.top().first] = e;
             idx_val.pop();
         }
 
         idx_val.push({ size - 1, heights[size - 1] });
         for (int i = size - 2; i >= 0; --i) {
             while (!idx_val.empty() && idx_val.top().second > heights[i]) {
-                firstit_lastit[idx_val.top().first].first = b + i + 1;
+                int area = (lastit[idx_val.top().first] - (b + i + 1)) * heights[idx_val.top().first];
+                if (ans < area) {
+                    ans = area;
+                }
                 idx_val.pop();
             }
             idx_val.push({ i, heights[i] });
         }
         while (!idx_val.empty()) {
-            firstit_lastit[idx_val.top().first].first = b;
+            int area = (lastit[idx_val.top().first] - b) * heights[idx_val.top().first];
+            if (ans < area) {
+                ans = area;
+            }
             idx_val.pop();
         }
-        for (int i = 0; i < size; ++i) {
-            if (ans < (firstit_lastit[i].second - firstit_lastit[i].first) * heights[i]) {
-                ans = (firstit_lastit[i].second - firstit_lastit[i].first) * heights[i];
-            }
-        }
-
-
         return ans;
     }
 };
