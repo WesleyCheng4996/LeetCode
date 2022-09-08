7b3c56b037b4133dce9ffbd15dbb593e48e6fab9
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-08 18:00:14 +0800

    Time: 488 ms (5.47%), Space: 89.2 MB (21.26%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -3,26 +3,26 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         int size = heights.size(), ans = 0;
         vector<int>::iterator b = heights.begin(), e = heights.end();
-        vector<int>last(size);
-        last[size - 1] = size;
+        vector<vector<int>::iterator>lastit(size);
+        lastit[size - 1] = e;
         stack<pair<int, int>>idx_val;
         idx_val.push({ 0, heights[0] });
         for (int i = 1; i < size; ++i) {
             while (!idx_val.empty() && idx_val.top().second > heights[i]) {
-                last[idx_val.top().first] = i;
+                lastit[idx_val.top().first] = b + i;
                 idx_val.pop();
             }
             idx_val.push({ i, heights[i] });
         }
         while (!idx_val.empty()) {
-            last[idx_val.top().first] = size;
+            lastit[idx_val.top().first] = e;
             idx_val.pop();
         }
 
         idx_val.push({ size - 1, heights[size - 1] });
         for (int i = size - 2; i >= 0; --i) {
             while (!idx_val.empty() && idx_val.top().second > heights[i]) {
-                int area = (last[idx_val.top().first] - i - 1) * heights[idx_val.top().first];
+                int area = (lastit[idx_val.top().first] - (b + i + 1)) * heights[idx_val.top().first];
                 if (ans < area) {
                     ans = area;
                 }
@@ -31,7 +31,7 @@ public:
             idx_val.push({ i, heights[i] });
         }
         while (!idx_val.empty()) {
-            int area = (last[idx_val.top().first]) * heights[idx_val.top().first];
+            int area = (lastit[idx_val.top().first] - b) * heights[idx_val.top().first];
             if (ans < area) {
                 ans = area;
             }
