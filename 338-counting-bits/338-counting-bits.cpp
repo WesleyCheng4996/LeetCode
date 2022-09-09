e7ad1111a694c661d5a1a4028e008ff69a15c4ca
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-09 22:36:03 +0800

    Time: 9 ms (53.36%), Space: 8.2 MB (44.08%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,19 @@
+template<size_t N>
+struct Bits{
+    int arr[N + 1];
+    constexpr Bits() :
+    arr() {
+        for(int i = 0; i <= N; ++i) {
+            arr[i] = __builtin_popcount(i);
+        }   
+    }
+};
+
+constexpr Bits<100000> b;
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        return vector<int>(b.arr, b.arr + n + 1);
+    }
+};
