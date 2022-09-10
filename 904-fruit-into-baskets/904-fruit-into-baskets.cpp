6c8ca3bcb7e490df6ea12f84d3012a9fa935350b
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-10 13:52:55 +0800

    Time: 297 ms (35.80%), Space: 66 MB (99.97%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int a = 0, b = 0, a_last_idx = -1, b_last_idx = -1, count = 0, ret = 0;
+        for(int i = 0, end = fruits.size(); i < end; ++i) {
+            if(a_last_idx == -1) {
+                a = fruits[i];
+                a_last_idx = i; 
+                ++count;
+            } else if(fruits[i] == a) {
+                ++count;
+                a_last_idx = i;
+            } else if(b_last_idx == -1) {
+                b = fruits[i];
+                b_last_idx = i;
+                ++count;
+            } else if(fruits[i] == b) {
+                ++count;
+                b_last_idx = i;
+            } else {
+                if(ret < count) {
+                    ret = count;
+                }
+                i = min(a_last_idx, b_last_idx);
+                a_last_idx = -1;
+                b_last_idx = -1;
+                count = 0;
+            }
+        }
+        if(ret < count) {
+            ret = count;
+        }
+        return ret;
+    }
+};
