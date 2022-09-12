d17dbed852cbdf2e19c946bb2aab6e5fde4045f2
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-13 00:00:56 +0800

    Time: 10 ms (57.09%), Space: 10.6 MB (27.39%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -4,23 +4,18 @@ public:
         int ret = 0, count = 0, idx1 = 0, idx2 = tokens.size() - 1;
         sort(tokens.begin(), tokens.end());
         while(idx1 <= idx2) {
-            if(count > 0) {
-                if(power < tokens[idx1]) {
-                    power += tokens[idx2--];
+            if(power >= tokens[idx1]) {
+                power -= tokens[idx1++];
+                ++count;
+            } else {
+                if(count == 0) {
+                    return ret;
+                } else {
+                     power += tokens[idx2--];
                     if(ret < count) {
                         ret = count;
                     }
                     --count;
-                } else {
-                    power -= tokens[idx1++];
-                    ++count;
-                }
-            } else {
-                if(power < tokens[idx1]) {
-                    return ret;
-                } else {
-                    power -= tokens[idx1++];
-                    ++count;
                 }
             }
         }
