a5f6afd13664b2bccf9744167371fec4432f9469
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-13 21:08:40 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (60.02%) - LeetHub

---
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int convertTime(string current, string correct) {
+        long long hour1 = stoll(current.substr(0, 2)), min1 = stoll(current.substr(3, 5));
+        long long hour2 = stoll(correct.substr(0, 2)), min2 = stoll(correct.substr(3, 5));
+        int ret = 0;
+        if(min1 > min2) {
+            --hour2;
+            min2 += 60;
+        }
+        ret += min2 - min1;
+        ret = ret / 15 + ret % 15 / 5 + ret % 5;
+        ret += hour2 - hour1;
+
+        return ret;
+    }
+};
