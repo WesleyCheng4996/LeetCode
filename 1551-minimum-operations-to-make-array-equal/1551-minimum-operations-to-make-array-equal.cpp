9a96138f900fe726d5dc4b6b5244d5cdc7130dc9
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-13 14:12:40 +0800

    Time: 4 ms (42.34%), Space: 5.9 MB (36.11%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,17 +1,3 @@
-template<size_t N>
-struct Array{
-    int arr[N + 1];
-    constexpr Array() :
-    arr() {
-        int m = 0, n = 0;
-        for(int i = 0; i < N; ++i) {
-            arr[i] = m++ * n;
-            arr[++i] = m * n++;
-        }
-    }
-};
-constexpr Array<10010> A;
-
 class Solution {
 public:
     int minOperations(int n) {
