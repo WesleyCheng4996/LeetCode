ceb5bb484bbdb962aa0ff6a2b02827d0940e8199
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-15 20:35:22 +0800

    Time: 26 ms (98.16%), Space: 10.3 MB (78.17%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -6,10 +6,11 @@ public:
         for(char ch: s) {
             if(st[ch] == 1) {
                 ++ret;
-                st = new char[128]();
+                memset(st + 97, 0, 26);
             }
             ++st[ch];   
         }
         return ret;
     }
 };
+
