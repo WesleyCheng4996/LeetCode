5014da6ae44ba1f678f4dd22c8b0eb237994cb75
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-15 12:48:08 +0800

    Time: 832 ms (30.04%), Space: 140.2 MB (84.93%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        sort(changed.begin(), changed.end());
         vector<int> ret;
         unordered_map<int, int>mp;
-        set<int> st(changed.begin(), changed.end());
+        sort(changed.begin(), changed.end());
         ret.reserve(changed.size());
         for (int x : changed) {
             ++mp[x];
         }
-        
-        if(st.count(0) == 1 ) {
-            if((mp[0] & 1) == 1) {
-                return vector<int>();
-            } else {
-                ret.insert(ret.end(), mp[0] >> 1, 0);
+        for (int x : changed) {
+            if (mp[x] == 0) {
+                continue;
             }
-            st.erase(0);
-        }
-        
-        for (int x : st) {
-            if(mp[x] > mp[x << 1]) {
+            --mp[x];
+            if (mp.count(x << 1) == 0 || mp[x << 1] == 0) {
                 return vector<int>();
-            } else {
-                mp[x << 1] -= mp[x];
-                ret.insert(ret.end(), mp[x], x);
             }
+            else {
+                --mp[x << 1];
+            }
+            ret.push_back(x);
         }
         return ret;
     }
 };
 
-static auto foo = std::ios::sync_with_stdio(false);
-static auto fo = std::cin.tie(nullptr);
