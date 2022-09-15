fb9fdb7bb77f4d0830b0db7fe753830de9755002
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-15 20:58:31 +0800

    Time: 14 ms (23.82%), Space: 10.5 MB (42.82%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        nums.push_back(INT_MAX);
-        vector<int>::iterator it = lower_bound(nums.begin(), nums.end(), 0);
-        return (*it == 0 ? 0 : pow(-1, it - nums.begin()));
+        int count = 0;
+        for(int x: nums) {
+            if(x == 0) {
+                return 0;
+            } else if(x < 0) {
+                ++count;
+            }
+        }
+        return pow(-1, count & 1);
     }
 };
