675afb4568e7bd07d8ac6ac223673b77a33e9103
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-15 14:54:54 +0800

    Time: 285 ms (5.23%), Space: 787.3 MB (25.29%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,18 @@
+struct Pow_2 {
+    long long arr[100000000];
+    Pow_2() :
+    arr() {
+        for(long long i = 0; i < 100000000; ++i) {
+            arr[i] = i * i; 
+        }
+    }
+};
+
+Pow_2 pow_2;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        return (upper_bound(pow_2.arr, pow_2.arr + 100000000, x) - pow_2.arr - 1);
+    }
+};
