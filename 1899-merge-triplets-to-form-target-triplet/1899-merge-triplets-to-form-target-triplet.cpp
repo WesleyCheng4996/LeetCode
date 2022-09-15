ce7306ee96f3759873370580e49561ab94579019
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-15 14:23:30 +0800

    Time: 1103 ms (17.27%), Space: 146.4 MB (72.76%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        int target_0 = -1, target_1 = -1, target_2 = -1;
+        for(vector<int> &vec: triplets) {
+            if(vec[0] == target[0] && vec[1] <= target[1] && vec[2] <= target[2]) {
+                target_0 = target[0], target_1 = max(target_1, vec[1]), target_2 = max(target_2, vec[2]);
+            } else if(vec[0] <= target[0] && vec[1] == target[1] && vec[2] <= target[2]) {
+                target_0 = max(target_0, vec[0]), target_1 = target[1], target_2 = max(target_2, vec[2]);
+            } else if(vec[0] <= target[0] && vec[1] <= target[1] && vec[2] == target[2]) {
+                target_0 = max(target_0, vec[0]), target_1 = max(target_1, vec[1]), target_2 = target[2];
+            }
+        }
+        return target_0 == target[0] && target_1 == target[1] && target_2 == target[2];
+    }
+};
