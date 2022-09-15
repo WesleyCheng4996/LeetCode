584da7ffab98e3cc7a9d9eb87cc249eb9ed3c1d0
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-15 12:42:10 +0800

    Time: 552 ms (75.91%), Space: 169.5 MB (16.39%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -4,22 +4,24 @@ public:
         vector<int> ret;
         unordered_map<int, int>mp;
         sort(changed.begin(), changed.end());
+        set<int> st(changed.begin(), changed.end());
+        st.erase(0);
         ret.reserve(changed.size());
         for (int x : changed) {
             ++mp[x];
         }
-        for (int x : changed) {
-            if (mp[x] == 0) {
-                continue;
-            }
-            --mp[x];
-            if (mp.count(x << 1) == 0 || mp[x << 1] == 0) {
+        if((mp[0] & 1) == 1) {
+            return vector<int>();
+        } else {
+            ret.insert(ret.end(), mp[0] >> 1, 0);
+        }
+        for (int x : st) {
+            if(mp[x] > mp[x << 1]) {
                 return vector<int>();
+            } else {
+                mp[x << 1] -= mp[x];
+                ret.insert(ret.end(), mp[x], x);
             }
-            else {
-                --mp[x << 1];
-            }
-            ret.push_back(x);
         }
         return ret;
     }
