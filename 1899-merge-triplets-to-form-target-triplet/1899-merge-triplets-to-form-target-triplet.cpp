d3605f95bb7e1e3865ef511730fa06f831649843
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-15 14:23:56 +0800

    Time: 1087 ms (19.92%), Space: 146.5 MB (44.11%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -5,9 +5,9 @@ public:
         for(vector<int> &vec: triplets) {
             if(vec[0] == target[0] && vec[1] <= target[1] && vec[2] <= target[2]) {
                 target_0 = target[0], target_1 = max(target_1, vec[1]), target_2 = max(target_2, vec[2]);
-            }  if(vec[0] <= target[0] && vec[1] == target[1] && vec[2] <= target[2]) {
+            } else if(vec[0] <= target[0] && vec[1] == target[1] && vec[2] <= target[2]) {
                 target_0 = max(target_0, vec[0]), target_1 = target[1], target_2 = max(target_2, vec[2]);
-            }  if(vec[0] <= target[0] && vec[1] <= target[1] && vec[2] == target[2]) {
+            } else if(vec[0] <= target[0] && vec[1] <= target[1] && vec[2] == target[2]) {
                 target_0 = max(target_0, vec[0]), target_1 = max(target_1, vec[1]), target_2 = target[2];
             }
         }
