0bcffdbbc36e82f3952361b9a678d0cb382d973a
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-15 20:33:09 +0800

    Time: 60 ms (88.66%), Space: 54.8 MB (39.69%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int partitionString(string s) {
-        char *st = new char[26]();
+        char *st = new char[128]();
         int ret = 1;
         for(char ch: s) {
-            if(st[ch - 'a'] == 1) {
+            if(st[ch] == 1) {
                 ++ret;
-                st = new char[26]();
+                st = new char[128]();
             }
-            ++st[ch - 'a'];   
+            ++st[ch];   
         }
         return ret;
     }
