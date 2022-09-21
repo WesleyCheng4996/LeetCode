a11e29504ff3ec12b6cca8a4c7b698b47a74357d
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-21 20:54:10 +0800

    Time: 210 ms (40.28%), Space: 45.4 MB (85.52%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<int>ret;
-        int s = accumulate(nums.begin(), nums.end(), 0, [](int x, int y) {return ((y & 1) == 0? x + y: x);});
-        ret.reserve(queries.size());
-        for(vector<int> &vec: queries) {
-            int &x = nums[vec[1]];
-            if((x & 1) == 0) {
-                s -= nums[vec[1]];
+        int now = 0;
+        for(int &i : nums) if(i % 2 == 0) now += i;
+        int n = nums.size();
+        vector<int> ans(n);
+        int i = 0;
+        for(auto& q : queries){
+            int ind = q[1], val = q[0];
+            int temp = nums[ind], tempp = temp + val;
+            if(temp % 2 == 0) {
+                if(val % 2 == 0) now += val;
+                else now -= temp;
             }
-            x += vec[0];
-            if((x & 1) == 0) {
-                s += x;
+            else{
+                if(val % 2 != 0) now += temp + val;
             }
-            ret.push_back(s);
+            nums[ind] += val;
+            ans[i++] = now;
         }
-        return (ret);
+        return ans;
     }
 };
