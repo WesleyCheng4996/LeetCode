f70ced290c85d56013665d7b76dd1289cedff262
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-23 13:27:16 +0800

    Time: 29 ms (97.28%), Space: 5.8 MB (95.65%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int concatenatedBinary(int n) {
-        unsigned long long ret = 0, mod = pow(10, 9) + 7;
+        unsigned long long ret = 0;
+        const unsigned int mod = pow(10, 9) + 7;
         for(int i = 1; i <= n; ++i) {
             ret <<= (32 - __builtin_clz(i));
             ret += i;
