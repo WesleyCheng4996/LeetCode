29b6e1aea47d1eafa30009b6996b78ddc1bced10
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-27 17:13:22 +0800

    Time: 117 ms (37.73%), Space: 7.6 MB (16.43%) - LeetHub

--- 470-implement-rand10-using-rand7/470-implement-rand10-using-rand7.cpp
+++ 470-implement-rand10-using-rand7/470-implement-rand10-using-rand7.cpp
@@ -4,7 +4,11 @@
 
 class Solution {
 public:
-    int rand10() {
-        return (rand()) % 10 + 1;
+  int rand10() {
+        int rand40 = 40;
+        while (rand40 >= 40) {
+            rand40 = (rand7() - 1) * 7 + rand7() - 1;
+        }
+        return rand40 % 10 + 1;
     }
 };
