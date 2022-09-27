ce56f435c2b1416e37e4374c42f8877e03a78072
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-27 16:22:01 +0800

    Time: 107 ms (23.22%), Space: 12.8 MB (88.39%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        int r = -2;
+        for (int i = 0, end = dominoes.size(); i < end; ++i) {
+            if (dominoes[i] == '.') {
+                continue;
+            }
+            else if (dominoes[i] == 'R') {
+                if (r >= 0) {
+                    for (int j = r + 1; j < i; ++j) {
+                        dominoes[j] = 'R';
+                    }
+                }
+                r = i;
+            }
+            else {
+                if (r == -2) {
+                    for (int j = 0; j < i; ++j) {
+                        dominoes[j] = 'L';
+                    }
+                }
+                else if (r == -1) {
+                    int c = i - 1;
+                    while (dominoes[c] == '.') {
+                        dominoes[c--] = 'L';
+                    }
+                    r = -1;
+                }
+                else {
+                    if (((r + i) & 1) == 0) {
+                        for (int j = r + 1, e = (r + i) >> 1; j < e; ++j) {
+                            dominoes[j] = 'R';
+                        }
+                        for (int j = ((r + i) >> 1) + 1; j < i; ++j) {
+                            dominoes[j] = 'L';
+                        }
+                    }
+                    else {
+                        for (int j = r + 1, e = (r + i) >> 1; j <= e; ++j) {
+                            dominoes[j] = 'R';
+                        }
+                        for (int j = ((r + i) >> 1) + 1; j < i; ++j) {
+                            dominoes[j] = 'L';
+                        }
+                    }
+                    r = -1;
+                }
+            }
+        }
+        if (r >= 0) {
+            for (int i = r + 1, end = dominoes.size(); i < end; ++i) {
+                dominoes[i] = 'R';
+            }
+        }
+        return dominoes;
+    }
+};
