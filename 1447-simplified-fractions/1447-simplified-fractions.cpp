36c94d32ab1bb04feda30adcd4e8f97b0ecde197
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-28 20:03:07 +0800

    Time: 68 ms (87.02%), Space: 22 MB (65.65%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.cpp
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -1,25 +1,26 @@
+template<int N>
 struct Table{
   constexpr Table():
     arr() {
-        for(int i = 0; i < 101; ++i) {
-            for(int j = 0; j < 101; ++j) {
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j) {
                 arr[i][j] = j;
             }
         }
-        for(int i = 2; i < 101; ++i) {
-            for(int j = 2; j < 101; ++j) {
+        for(int i = 2; i < N; ++i) {
+            for(int j = 2; j < N; ++j) {
                 if(i % j == 0) {
-                    for(int k = j; k < 101; k += j) {
+                    for(int k = j; k < N; k += j) {
                         arr[i][k] = 0;
                     }
                 }
             }
         }
     }
-    int arr[101][101];
+    int arr[N][N];
 };
 
-constexpr Table table;
+constexpr Table<101> table;
 
 class Solution {
 public:
