fcfb10755282ef3e594c14b6809cf71635dfce8b
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-28 22:20:29 +0800

    Time: 5 ms (92.35%), Space: 5.9 MB (90.54%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -0,0 +1,25 @@
+struct ugly {
+    ugly() :
+        arr() {
+        arr[0] = 1;
+        int i = 0, j = 0, k = 0, idx = 1;
+        while (idx < 1691) {
+            int num1 = arr[i] << 1, num2 = arr[j] * 3, num3 = arr[k] * 5;
+            arr[idx] = min(num1, min(num2, num3));
+            if (arr[idx] == num1) ++i;
+            if (arr[idx] == num2) ++j;
+            if (arr[idx] == num3) ++k;
+            ++idx;
+        }
+    }
+    int arr[1691];
+};
+
+ugly u;
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        return u.arr[n - 1];
+    }
+};
