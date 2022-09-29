4a976889f13574974bfcd85703571f24c334fff1
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-29 15:49:35 +0800

    Time: 4 ms (73.18%), Space: 6.5 MB (24.38%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,8 +1,9 @@
 struct Sum {
     constexpr Sum() :
     arr() {
+        arr[0] = 1;
         for(int i = 1; i < 100000; ++i) {
-            arr[i] = arr[i - 1] + i; 
+            arr[i] = arr[i - 1] + (i + 1); 
         }
     }
     long long arr[100000];
@@ -13,6 +14,6 @@ constexpr Sum s;
 class Solution {
 public:
     int arrangeCoins(int n) {
-        return upper_bound(s.arr, s.arr + 100000, n) - s.arr - 1;
+        return upper_bound(s.arr, s.arr + 100000, n) - s.arr;
     }
 };
