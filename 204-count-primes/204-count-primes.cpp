e10f9c8436ca5524a9ddcbe93827241246e21239
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-29 11:31:48 +0800

    Time: 15 ms (99.87%), Space: 7.6 MB (99.42%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -2,20 +2,20 @@
 struct Prime {
     Prime() :
     arr() {
-        unsigned int isPrime[78126] = { 0 }, idx = 1;
-        for (int i = 0; i < 78126; ++i) {
+        unsigned int isPrime[78125] = { 0 }, idx = 1;
+        for (int i = 0; i < 78125; ++i) {
             --isPrime[i];
         }
-        for (int i = 3; i < 2237; i += 2) {
-            if ((isPrime[i >> 6]) & (1 << ((i % 64) >> 1))) {
+        for (int i = 3; i < 2222; i += 2) {
+            if ((isPrime[i >> 6]) & (1 << ((i & 63) >> 1))) {
                 for (int j = i + i + i; j < 5000000; j += i + i) {
-                    isPrime[j >> 6] &= ~(1 << ((j % 64) >> 1));
+                    isPrime[j >> 6] &= ~(1 << ((j & 63) >> 1));
                 }
             }
         }
         arr[0] = 2;
         for (int i = 3; i < 5000000; i += 2) {
-            if (isPrime[i >> 6] & (1 << ((i % 64) >> 1))) {
+            if (isPrime[i >> 6] & (1 << ((i & 63) >> 1))) {
                 arr[idx++] = i;
             }
         }
