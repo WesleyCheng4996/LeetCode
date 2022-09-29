3171ceb8b137b060c35262980bc91e594b77bfd1
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-29 13:01:45 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (25.73%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        unsigned int ret = 0, cin = 0;
-        for(int i = 0; i < 32; ++i) {
-            ret |= (((a >> i) & 1) ^ ((b >> i) & 1) ^ cin) << i;
-            cin = (((a >> i) & 1) & ((b >> i) & 1)) || (((a >> i) & 1) & cin) ||(((b >> i) & 1) & cin);
-        }
-        return ret;
+        
+        return a + b;
     }
 };
