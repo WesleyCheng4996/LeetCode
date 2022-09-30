f0715d3f4ee93f8dd9d752d513d26c9525d03148
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-30 18:40:25 +0800

    Time: 64 ms (23.36%), Space: 9.9 MB (64.74%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,9 +3,7 @@ public:
     int maxVowels(string s, int k) {
         int ret = 0, len = 0, val[128] = { 0 };
         val['a'] = val['e'] = val['i'] = val['o'] = val['u'] = 1;
-        for(int i = 0; i < k; ++i) {
-            len += val[s[i]];
-        }
+        len = accumulate(s.begin(), s.begin() + k, 0, [&](int sum, int ch){return sum + val[ch];});
         for(string::iterator it = s.begin(), itt = s.begin() + k; itt < s.end(); ++it, ++itt) {
             if(val[*it] ^ val[*itt]) {
                 if(val[*it]) {
