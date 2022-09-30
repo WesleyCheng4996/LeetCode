6686908a6ac22fad8c5dcc74f1c2ce9fc2c0fa78
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-30 15:47:57 +0800

    Time: 4 ms (59.31%), Space: 7.3 MB (52.17%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        set<vector<int>>st;
-        st.insert(vector<int>{});
-        st.insert(nums);
+        vector<vector<int>>st;
+        st.push_back(vector<int>{});
+        st.push_back(nums);
         
         for(int i = 0, end = nums.size(); i < end; ++i) {
             bfs(nums, st, i);
@@ -11,7 +11,7 @@ public:
         vector<vector<int>>ret(st.begin(), st.end());
         return ret;
     }
-    void bfs(vector<int> nums, set<vector<int>> &st, int idx) {
+    void bfs(vector<int> nums, vector<vector<int>> &st, int idx) {
         if(nums.size() == 1) {
             return;
         }
@@ -19,6 +19,6 @@ public:
         for(int i = idx, end = nums.size(); i < end; ++i) {
             bfs(nums, st, i);
         }
-        st.insert(nums);
+        st.push_back(nums);
     }
 };
