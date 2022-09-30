f5196ed7aba6e2f46298fbdc7d916aff1efb767b
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-09-30 15:28:57 +0800

    Time: 5 ms (21.79%), Space: 6 MB (72.57%) - LeetHub

--- 2409-count-days-spent-together/2409-count-days-spent-together.cpp
+++ 2409-count-days-spent-together/2409-count-days-spent-together.cpp
@@ -3,10 +3,10 @@ class Solution {
 public:
     int countDaysTogether(string arriveAlice, string leaveAlice, string arriveBob, string leaveBob) {
         bool dayA[366] = { false };
-        bool *ptrAa = dayA + dayM[stoi(arriveAlice.substr(0, 2))] + stoi(arriveAlice.substr(3, 5));
-        bool *ptrAb = dayA + dayM[stoi(leaveAlice.substr(0, 2))] + stoi(leaveAlice.substr(3, 5));
-        bool *ptrBa = dayA + dayM[stoi(arriveBob.substr(0, 2))] + stoi(arriveBob.substr(3, 5));
-        bool *ptrBb = dayA + dayM[stoi(leaveBob.substr(0, 2))] + stoi(leaveBob.substr(3, 5));
+        int ptrAa = dayM[stoi(arriveAlice.substr(0, 2))] + stoi(arriveAlice.substr(3, 5));
+        int ptrAb = dayM[stoi(leaveAlice.substr(0, 2))] + stoi(leaveAlice.substr(3, 5));
+        int ptrBa = dayM[stoi(arriveBob.substr(0, 2))] + stoi(arriveBob.substr(3, 5));
+        int ptrBb = dayM[stoi(leaveBob.substr(0, 2))] + stoi(leaveBob.substr(3, 5));
         return (ptrAb < ptrBa || ptrBb <ptrAa ? 0 : ((ptrAb <= ptrBb ? ptrAb : ptrBb) - (ptrAa > ptrBa ? ptrAa : ptrBa) + 1));
     }
 };
