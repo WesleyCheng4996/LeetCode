c4d90d297cc1770123ede6d36e29e2d774d21350
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-02 19:20:44 +0800

    Time: 197 ms (27.49%), Space: 37.9 MB (88.20%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int>result(arr.size() + 1);
-        result.back() = accumulate(arr.begin(), arr.end(), 0, [](int sum, int x) { return sum ^ x; });
-        for(int i = arr.size() - 1; i >= 0; --i) {
-            result[i] = result[i + 1] ^ arr[i];
+        for(int i = 0, end = arr.size(), k = 0; i < end; ++i) {
+            result[i + 1] = k ^= arr[i];
         }
         vector<int>ret;
         ret.reserve(queries.size());
