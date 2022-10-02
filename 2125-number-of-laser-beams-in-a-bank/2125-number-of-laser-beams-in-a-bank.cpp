c89ce3129fe46750b617a3e0626349d725ccce5f
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-02 23:15:06 +0800

    Time: 101 ms (98.01%), Space: 23.3 MB (49.21%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int *count = new int[bank.size()]();
+        for(int i = 0, end = bank.size(); i < end; ++i) {
+            count[i] = accumulate(bank[i].begin(), bank[i].end(), 0, 
+                                  [](int sum, int x){return sum + x - '0'; }
+                                 );
+        }
+        int last = 0;
+        return accumulate(count, count + bank.size(), 0, 
+                          [&](int sum, int x)
+                          { 
+                              if(x != 0) {
+                                  sum += (last * x);
+                                  last = x;
+                              }
+                              return sum;
+                          });
+    }
+};
