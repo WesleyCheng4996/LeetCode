27fc4bef7e6227c79205a65461fc1607914e2df7
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-03 13:53:37 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (96.04%) - LeetHub

---
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
@@ -0,0 +1,26 @@
+struct Fib {
+    int arr[45];
+    constexpr Fib() :
+    arr() {
+        arr[0] = 1;
+        arr[1] = 1;
+        for(int i = 2; i < 45; ++i) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+    }
+};
+constexpr Fib fib;
+
+class Solution {
+public:
+    int findMinFibonacciNumbers(int k) {
+        int ret = 0;
+        for(int i = 44; k; --i) {
+            if(k >= fib.arr[i]) {
+                ++ret;
+                k -= fib.arr[i++];
+            }
+        }
+        return ret;
+    }
+};
