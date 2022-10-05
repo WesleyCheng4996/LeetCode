afd28fcb616f9d2421feb41d0af63146ca34cfc2
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-05 15:12:24 +0800

    Time: 417 ms (16.38%), Space: 39.6 MB (12.93%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
-        long long ret = 0, minnum = abs(matrix[0][0]), sign = 0;
+        int minnum = abs(matrix[0][0]), sign = 0;
         return accumulate(matrix.begin(), matrix.end(), 0LL, [&](long long sum, vector<int>vec) {
             return sum += accumulate(vec.begin(), vec.end(), 0LL, [&](long long sum, int num) {
                 if(num < 0) {
