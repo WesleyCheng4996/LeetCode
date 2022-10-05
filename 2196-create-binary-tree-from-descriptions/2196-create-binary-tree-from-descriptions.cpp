83b37498c1cf513b05782f9888ad9ab56d02f444
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-05 15:36:54 +0800

    Time: 2031 ms (30.70%), Space: 277.7 MB (84.73%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, int[2]>nodes;
+        unordered_map<int, bool>ishead;
+        int head;
+        for (vector<int>& vec : descriptions) {
+            nodes[vec[0]][vec[2]] = vec[1];
+            if(ishead.count(vec[0]) == 0) {
+                ishead[vec[0]] = true;
+            }
+            ishead[vec[1]] = false;
+        }
+        for(auto& [val, b] : ishead) {
+            if(b) {
+                head = val;
+                break;
+            }
+        }
+        TreeNode* ret = new TreeNode(head);
+        creatNode(ret, nodes);
+        return ret;
+    }
+    
+    void creatNode(TreeNode* root, unordered_map<int, int[2]>& mp) {
+        if(mp[root->val][0]) {
+            root->right = new TreeNode(mp[root->val][0]);
+            creatNode(root->right, mp);
+        }
+        if(mp[root->val][1]) {
+            root->left = new TreeNode(mp[root->val][1]);
+            creatNode(root->left, mp);
+        }
+    }
+    
+};
