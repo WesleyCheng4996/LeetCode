5075df507d12e5f3408a617e439b7f02228ee8f5
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-05 15:52:00 +0800

    Time: 2108 ms (25.16%), Space: 283.1 MB (56.37%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -12,23 +12,34 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int, int[2]>nodes;
-        unordered_map<int, bool>ishead;
-        int head;
+        unordered_map<int, TreeNode*>nodes;
+        unordered_set<int> root;
+        unordered_set<int>son;
+        nodes.reserve(descriptions.size());
+        root.reserve(descriptions.size());
+        son.reserve(descriptions.size());
         for (vector<int>& vec : descriptions) {
-            nodes[vec[0]][vec[2]] = vec[1];
-            if(ishead.count(vec[0]) == 0) {
-                ishead[vec[0]] = true;
+            if(nodes.count(vec[0]) == 0) {
+                nodes.insert(pair<int, TreeNode*>(vec[0], new TreeNode(vec[0])));
             }
-            ishead[vec[1]] = false;
+            if(nodes.count(vec[1]) == 0) {
+                nodes.insert(pair<int, TreeNode*>(vec[1], new TreeNode(vec[1])));
+            }
+            if(vec[2]) {
+                nodes[vec[0]]->left = nodes[vec[1]];
+            } else {
+                nodes[vec[0]]->right = nodes[vec[1]];
+            }
+            root.insert(vec[0]);
+            son.insert(vec[1]);
         }
-        for(auto& [val, b] : ishead) {
-            if(b) {
-                head = val;
-                break;
+        
+        for(int x : root) {
+            if(son.count(x) == 0) {
+                return nodes[x];
             }
         }
-        return creatNode(new TreeNode(head), nodes);;
+        return nullptr;
     }
     
     TreeNode* creatNode(TreeNode* root, unordered_map<int, int[2]>& mp) {
