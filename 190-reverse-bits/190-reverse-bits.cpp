49dfd16a2710daf1c12550568f90f08ee9db4fad
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-05 18:50:21 +0800

    Time: 7 ms (26.10%), Space: 6 MB (51.12%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        for(int i = 0; i < 16; ++i) {
+            if(((n >> i) & 1) ^ ((n >> (31 - i)) & 1)) {
+                n ^= ((1 << i) | (1 << (31 - i)));
+            }
+        }
+        return n;
+    }
+};
