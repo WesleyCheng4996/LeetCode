dad181da13d3bdac377948959d3bee42f31cf9a6
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-06 00:53:27 +0800

    Time: 30 ms (56.38%), Space: 11.6 MB (65.82%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool makeEqual(vector<string>& words) {
-        int count[128] = { 0 };
+        int count[128] = { 0 }, sz = words.size();
         for(string& vec : words) {
             for(char x : vec) {
                 ++count[x];
             }
         }
         for(int i = 97; i < 123; ++i) {
-            if(count[i] % words.size()) {
+            if(count[i] % sz) {
                 return false;
             }
         }
