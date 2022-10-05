29f4fd1c4bfec43b96f1fe88e46b1d27c91043c8
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-05 15:54:50 +0800

    Time: 2045 ms (29.52%), Space: 274.5 MB (88.93%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -12,15 +12,15 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        map<int, TreeNode*>nodes;
+        TreeNode* nodes[100001] = { 0 };
         set<int> root;
         set<int>son;
         for (vector<int>& vec : descriptions) {
-            if(nodes.count(vec[0]) == 0) {
-                nodes.insert(pair<int, TreeNode*>(vec[0], new TreeNode(vec[0])));
+            if(nodes[vec[0]] == 0) {
+                nodes[vec[0]] = new TreeNode(vec[0]);
             }
-            if(nodes.count(vec[1]) == 0) {
-                nodes.insert(pair<int, TreeNode*>(vec[1], new TreeNode(vec[1])));
+            if(nodes[vec[1]] == 0) {
+                nodes[vec[1]] = new TreeNode(vec[1]);
             }
             if(vec[2]) {
                 nodes[vec[0]]->left = nodes[vec[1]];
