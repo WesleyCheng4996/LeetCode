68b9bc00bd7fd8e470936c744502c55668c5552d
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-05 16:46:10 +0800

    Time: 45 ms (5.17%), Space: 8 MB (43.16%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/657-robot-return-to-origin.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool judgeCircle(string moves) {
         unordered_map<char, int>mp;
-        for (char ch : moves) {
-            ++mp[ch];
-        }
+        for (char ch : moves) ++mp[ch];
         return !((mp['U'] - mp['D']) || (mp['L'] - mp['R']));
     }
 };
