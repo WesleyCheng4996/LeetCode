5e4645d4e25d958ab19bfabd3504dead7f639480
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-05 18:44:34 +0800

    Time: 434 ms (23.42%), Space: 149.9 MB (25.98%) - LeetHub

---
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
+        int minr = min(startPos[0], homePos[0]), maxr = max(startPos[0], homePos[0]);
+        int minc = min(startPos[1], homePos[1]), maxc = max(startPos[1], homePos[1]);
+        
+        return accumulate(rowCosts.begin() + minr, rowCosts.begin() + maxr + 1, 0) + accumulate(colCosts.begin() + minc, colCosts.begin() + maxc + 1, 0) - rowCosts[startPos[0]] - colCosts[startPos[1]];
+    }
+};
