3c92ac50a66af6d4304f9162f79426998f57bb9d
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-06 14:50:21 +0800

    Time: 261 ms (15.27%), Space: 34.3 MB (5.59%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isValid(string str) {
+        regex reg("abc");
         int sz = 0;
         while(sz != str.size()) {
             sz = str.size();
-            str = regex_replace(str, regex("abc"), "");
+            str = regex_replace(str, reg, "");
         }
         return str.empty();
     }
