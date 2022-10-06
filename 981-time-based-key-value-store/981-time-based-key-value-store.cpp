7a188472101d660a74ca815fb99db65d8cb53c53
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-06 13:22:21 +0800

    Time: 443 ms (77.74%), Space: 128.7 MB (45.32%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -13,7 +13,7 @@ class TimeMap {
 public:
     TimeMap() {
         ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+        cin.tie(nullptr);
     }
     
     void set(string key, string value, int timestamp) {
