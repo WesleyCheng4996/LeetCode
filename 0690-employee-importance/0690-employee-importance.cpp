1591749529c169216ae0f6eec9823eb7187aecd6
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-12 13:55:56 +0800

    Time: 68 ms (35.97%), Space: 24.4 MB (17.24%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -20,9 +20,6 @@ public:
         return recur(employees, id, id_to_idx);
     }
     int recur(vector<Employee*> employees, int id, int idtoidx[2001]) {
-        if (employees[idtoidx[id]]->subordinates.empty()) {
-            return employees[idtoidx[id]]->importance;
-        }
         int ret = employees[idtoidx[id]]->importance;
         for(int i = 0, end = employees[idtoidx[id]]->subordinates.size(); i < end; ++i) {
             ret += recur(employees, employees[idtoidx[id]]->subordinates[i], idtoidx);
