26b2557ab793873f831563acfe429866ccc90204
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-14 13:31:05 +0800

    Time: 105 ms (91.24%), Space: 55.2 MB (6.86%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,3 +4,5 @@ public:
         return unordered_set<int>(nums.begin(), nums.end()).size() != nums.size(); 
     }
 };
+static auto s = ios_base::sync_with_stdio(false);
+static auto ss = cin.tie(nullptr);
