8069ef3c7bb20493cbdd10682a766c09a48b00cf
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-14 23:36:18 +0800

    Time: 28 ms (77.09%), Space: 17 MB (24.44%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -4,3 +4,5 @@ public:
         return accumulate(nums.begin(), nums.end(), 0, [](int sum, int x) { return sum ^ x; });
     }
 };
+static auto s = ios_base::sync_with_stdio(false);
+static auto ss = cin.tie(nullptr);
