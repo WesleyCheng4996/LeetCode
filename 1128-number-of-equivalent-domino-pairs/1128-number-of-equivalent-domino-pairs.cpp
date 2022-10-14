b29abbfe2635493dbe25750431fb5980b01014e0
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-14 23:31:09 +0800

    Time: 49 ms (96.50%), Space: 22 MB (86.76%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.cpp
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.cpp
@@ -14,13 +14,13 @@ constexpr Table t;
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& d) {
-        map<vector<int>, int> mp;
+        map<int, int> mp;
         int ret = 0;
         for(vector<int> &x : d) {
             if(x[0] > x[1]) {
                 swap(x[0], x[1]);
             }
-            ++mp[x];
+            ++mp[x[0] * 10 + x[1]];
         }
         for(auto&[key, val] : mp) {
             ret += t.arr[val];
