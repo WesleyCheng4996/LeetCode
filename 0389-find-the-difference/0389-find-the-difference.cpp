8f5a6d7e7f1ed062f5743cabec5ff0600423f9ce
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-14 23:46:45 +0800

    Time: 0 ms (100.00%), Space: 6.9 MB (10.14%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char ret = 0;
         s += t;
-        for(char c : s) {
-            ret ^= c;
-        }
-        return ret;
+        return accumulate(s.begin(), s.end(), 0, [](char sum, char x) { return sum ^ x; });
     }
 };
