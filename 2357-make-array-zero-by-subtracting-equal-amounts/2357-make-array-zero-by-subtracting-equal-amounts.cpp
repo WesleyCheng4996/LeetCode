9caa698317acac2f56344f18abcfc626f5152752
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-14 13:29:31 +0800

    Time: 0 ms (100.00%), Space: 8.6 MB (12.98%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        set<int>st(nums.begin(), nums.end()); 
+        unordered_set<int>st(nums.begin(), nums.end()); 
         return st.size() - st.count(0);
     }
 };
