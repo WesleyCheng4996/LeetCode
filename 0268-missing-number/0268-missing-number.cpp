48a81021c5f98d7e47088e116036ed2a5c67eb5b
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-14 23:43:36 +0800

    Time: 21 ms (90.46%), Space: 18.1 MB (20.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -16,3 +16,6 @@ public:
         return t.arr[nums.size()] ^ accumulate(nums.begin(), nums.end(), 0, [](int sum, int x) { return sum ^ x; });
     }
 };
+
+static auto s = ios_base::sync_with_stdio(false);
+static auto ss = cin.tie(nullptr);
