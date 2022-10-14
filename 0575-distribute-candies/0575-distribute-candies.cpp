54f8a28394fdb84f3480dfa8d033f0b3e73b4edb
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-14 22:16:07 +0800

    Time: 479 ms (55.63%), Space: 110.4 MB (75.24%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -4,3 +4,5 @@ public:
         return min((int)unordered_set<int>(candyType.begin(), candyType.end()).size(), (int)(candyType.size() >> 1));
     }
 };
+static auto s = ios_base::sync_with_stdio(false);
+static auto ss = cin.tie(nullptr);
