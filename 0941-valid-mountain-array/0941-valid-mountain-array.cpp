4fa24a9b2c56ec14baddfc22750dd2e38116f0a7
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-19 14:18:07 +0800

    Time: 81 ms (21.72%), Space: 22.3 MB (79.13%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        if(arr.size() < 3) {
-            return false;
-        }
         auto max_it = max_element(arr.begin(), arr.end());
         return max_it != arr.begin() && max_it != arr.end() - 1 && all_of(arr.begin(), max_it, [](int& i) { return i < *(&i + 1); }) && all_of(max_it, arr.end() - 1, [](int& i) { return i > *(&i + 1); });
     }
