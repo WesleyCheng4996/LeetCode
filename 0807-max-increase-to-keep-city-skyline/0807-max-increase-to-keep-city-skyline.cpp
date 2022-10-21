cf124f33b55ae849913264e3de05d2834dbd4857
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-21 18:57:43 +0800

    Time: 16 ms (54.27%), Space: 10.1 MB (32.17%) - LeetHub

---
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int* rowsmax = new int[grid.size()]();
+        int* colsmax = new int[grid[0].size()]();
+        int ret = 0;
+        for(int i = 0; i < grid.size(); ++i) {
+            int rowmax = grid[i][0];
+            for(int j = 0; j < grid[0].size(); ++j) {
+                if(rowmax < grid[i][j]) {
+                    rowmax = grid[i][j];
+                }
+                if(colsmax[j] < grid[i][j]) {
+                    colsmax[j] = grid[i][j];
+                }
+            }
+            rowsmax[i] = rowmax;
+        }
+        for(int i = 0; i < grid.size(); ++i) {
+            for(int j = 0; j < grid[0].size(); ++j) {
+                ret += min(rowsmax[i], colsmax[j]) - grid[i][j];
+            }
+        }
+        return ret;
+    }
+};
