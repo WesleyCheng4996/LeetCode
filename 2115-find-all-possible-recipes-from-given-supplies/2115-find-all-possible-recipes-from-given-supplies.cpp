30ad1d76ce366f7ac0e916999385004c6c8b9a1e
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-23 18:32:25 +0800

    Time: 373 ms (92.38%), Space: 140.3 MB (46.28%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool cook(const string& food, unordered_map<string, vector<string>>& food_ingredients, unordered_set<string>& haved, unordered_map<string, bool>& food_visited) {
+        if (haved.count(food)) {
+            return true;
+        }
+        if (food_ingredients.count(food) == 0 || (food_ingredients.count(food) && food_visited[food])) {
+            return false;
+        }
+        food_visited[food] = true;
+
+        bool cancook = true;
+        for (string& ingredient : food_ingredients[food]) {
+            cancook &= (haved.count(ingredient) || cook(ingredient, food_ingredients, haved, food_visited));
+        }
+        if (cancook) {
+            haved.insert(food);
+        }
+        return cancook;
+    }
+    vector<string> findAllRecipes(vector<string> recipes, vector<vector<string>> ingredients, vector<string> supplies) {
+        unordered_map<string, vector<string>>food_ingredients;
+        unordered_map<string, bool>food_visited;
+        for (int i = 0; i < recipes.size(); ++i) {
+            food_ingredients[recipes[i]] = ingredients[i];
+            food_visited[recipes[i]] = false;
+        }
+        unordered_set<string>sup(supplies.begin(), supplies.end());
+        vector<string>ret;
+        for (string& food : recipes) {
+            bool cancook = true;
+            for (string& need : food_ingredients[food]) {
+                if ((cancook &= (sup.count(need) || cook(need, food_ingredients, sup, food_visited))) == false) {
+                    break;
+                }
+            }
+            if (cancook) {
+                ret.push_back(food);
+            }
+        }
+        return ret;
+    }
+};
