c9c639610887139c477b102e72e1ce21336d70de
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-27 16:01:45 +0800

    Time: 536 ms (21.58%), Space: 46.8 MB (29.97%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindromic(const string& s) {
+        int half_sz = s.size() >> 1;
+        for(int i = 0; i < half_sz; ++i) {
+            if(s[i] != s[s.size() - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        string ret = s.substr(0, 1);
+        for(int i = 0; i < s.size(); ++i) {
+            string substr = s.substr(i, s.size() - i);
+            while(substr.size() > ret.size()) {
+                if(substr[0] == substr.back() && isPalindromic(substr)) {
+                    ret = substr;
+                    break;
+                }
+                substr.pop_back();
+            }
+        }
+        return ret;
+    }
+};
