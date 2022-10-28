59e0e7d8df4a2a648b0eb949973530f71e6934aa
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-28 15:07:19 +0800

    Time: 159 ms (100.00%), Space: 119.9 MB (93.93%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -16,3 +16,7 @@ public:
         return vector<int>(stk, ptr);
     }
 };
+
+static auto s = ios_base::sync_with_stdio(false);
+static auto ss = cin.tie(nullptr);
+static auto sss = cout.tie(nullptr);
