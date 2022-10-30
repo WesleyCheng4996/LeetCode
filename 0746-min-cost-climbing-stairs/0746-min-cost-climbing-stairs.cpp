84b75e130159609cc59156f8725bf1a1edf1f2c4
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-31 00:32:59 +0800

    Time: 23 ms (10.42%), Space: 13.7 MB (81.37%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        for(int i=2;i<cost.size();i++) //here we try to store the least sum at the end 2.
-        {
-            cost[i]+=min(cost[i-1],cost[i-2]);
+        for(int i = 2; i < cost.size(); i++) {
+            cost[i] += min(cost[i-1], cost[i-2]);
         }
-        return min(cost[cost.size()-1],cost[cost.size()-2]);//At the end the final answer depends on the last two costs
+        return min(cost[cost.size() - 1], cost[cost.size() - 2]);//At the end the final answer depends on the last two costs
     }
 };
