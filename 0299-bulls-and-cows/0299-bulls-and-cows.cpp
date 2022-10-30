96603133e03698a6058800e4960d4bac19e1de07
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-30 14:56:01 +0800

    Time: 4 ms (85.38%), Space: 6.6 MB (43.09%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -17,3 +17,6 @@ public:
         return to_string(A) + "A" + to_string(B) + "B";
     }
 };
+static auto s = ios_base::sync_with_stdio(false);
+static auto ss = cin.tie(nullptr);
+static auto sss = cout.tie(nullptr);
