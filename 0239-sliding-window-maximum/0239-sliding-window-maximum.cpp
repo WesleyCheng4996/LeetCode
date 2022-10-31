254f4cc2014511ae362f236b37d54673c3442c8f
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-31 15:12:00 +0800

    Time: 686 ms (38.15%), Space: 175.7 MB (15.94%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         map<int, int>val_count;
-        map<int, int>::iterator it;
         vector<int>ret;
         for(int i = 0; i < k; ++i) {
             ++val_count[nums[i]];
         }
-        ret.push_back(val_count.rbegin()->first);
-        for(int i = k; i < nums.size(); ++i) {
-            it = val_count.find(nums[i - k]);
-            --(it->second);
-            if((it->second) == 0) {
-                val_count.erase(it);
+        ret.push_back((--val_count.end())->first);
+        for(int i = 0, j = k; j < nums.size(); ++i, ++j) {
+            --val_count[nums[i]];
+            if(val_count[nums[i]] == 0) {
+                val_count.erase(nums[i]);
             }
-            ++val_count[nums[i]];
-            ret.push_back(val_count.rbegin()->first);
+            ++val_count[nums[j]];
+            ret.push_back((--val_count.end())->first);
         }
         return ret;
     }
 };
-
-static auto s = ios_base::sync_with_stdio(false);
-static auto ss = cin.tie(nullptr);
-static auto sss = cout.tie(nullptr);
