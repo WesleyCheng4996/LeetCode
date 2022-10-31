c2d44612b361f06a462b2e383b90e8a11123d807
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-31 15:10:30 +0800

    Time: 851 ms (17.52%), Space: 175.4 MB (17.51%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         map<int, int>val_count;
+        map<int, int>::iterator it;
         vector<int>ret;
         for(int i = 0; i < k; ++i) {
             ++val_count[nums[i]];
         }
         ret.push_back(val_count.rbegin()->first);
         for(int i = k; i < nums.size(); ++i) {
-            --val_count[nums[i - k]];
-            if(val_count[nums[i - k]] == 0) {
-                val_count.erase(nums[i - k]);
+            it = val_count.find(nums[i - k]);
+            --(it->second);
+            if((it->second) == 0) {
+                val_count.erase(it);
             }
             ++val_count[nums[i]];
             ret.push_back(val_count.rbegin()->first);
