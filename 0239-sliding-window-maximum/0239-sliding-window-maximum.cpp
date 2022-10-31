43868f1a3491872d618c2142e2a744e0b7e538d1
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-10-31 15:04:05 +0800

    Time: 1295 ms (6.42%), Space: 175.7 MB (14.48%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        map<int, int>val_count;
+        vector<int>ret;
+        for(int i = 0; i < k; ++i) {
+            ++val_count[nums[i]];
+        }
+        ret.push_back((--val_count.end())->first);
+        for(int i = 0, j = k; j < nums.size(); ++i, ++j) {
+            --val_count[nums[i]];
+            if(val_count[nums[i]] == 0) {
+                val_count.erase(nums[i]);
+            }
+            ++val_count[nums[j]];
+            ret.push_back((--val_count.end())->first);
+        }
+        return ret;
+    }
+};
