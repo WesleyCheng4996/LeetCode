30cf294aecdc73c030110f0c93603d9ecc860b60
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-11-01 21:16:55 +0800

    Time: 3 ms (56.71%), Space: 6.5 MB (29.03%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<vector<string>> h(100, vector<string>());
+        vector<vector<string>> m(100, vector<string>());
+        vector<string>ret;
+        for(int i = 0; i < 12; ++i) {
+            h[__builtin_popcount(i)].push_back(to_string(i));
+        }
+        for(int j = 0; j < 60; ++j) {
+            m[__builtin_popcount(j)].push_back((j < 10 ? "0" : "") + to_string(j));
+        }
+        for(int i = 0; i <= turnedOn; ++i) {
+            for(string hh : h[i]) {
+                for(string mm : m[turnedOn - i]) {
+                    ret.push_back(hh + ":" + mm);
+                }
+            }
+        }
+        return ret;
+    }
+};
