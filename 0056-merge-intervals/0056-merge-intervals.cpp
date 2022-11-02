fb050caa6883c0f61d7ca928c0eefcc0d8508415
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-11-02 17:00:49 +0800

    Time: 76 ms (62.02%), Space: 19.3 MB (41.11%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>>ret;
+        stack<int>stk;
+        int begin = intervals[0][0];
+        stk.push(intervals[0][1]);
+        for(int i = 1; i < intervals.size(); ++i) {
+            int end;
+            if(!stk.empty() && intervals[i][0] > stk.top()) {
+                end = stk.top();
+            }
+            while(!stk.empty() && intervals[i][0] > stk.top()) {
+                end = max(end, stk.top());
+                stk.pop();
+            }
+            if(stk.empty()) {
+                ret.push_back(vector<int>{begin, end});
+                begin = intervals[i][0];
+            }
+            stk.push(intervals[i][1]);
+        }
+        if(!stk.empty()) {
+            int end = stk.top();
+             while(!stk.empty()) {
+                end = max(end, stk.top());
+                stk.pop();
+            }
+            ret.push_back(vector<int>{begin, end});
+        }
+        return ret;
+    }
+};
