d35238aab102fd048f5219d6cd17521ded788e94
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-11-03 19:18:31 +0800

    Time: 64 ms (72.02%), Space: 28.5 MB (40.89%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -4,3 +4,7 @@ public:
         return accumulate(nums.begin(), nums.end(), 0) - (*min_element(nums.begin(), nums.end()) * nums.size());           
     }
 };
+
+static auto s = ios_base::sync_with_stdio(false);
+static auto ss = cin.tie(nullptr);
+static auto sss = cout.tie(nullptr);
