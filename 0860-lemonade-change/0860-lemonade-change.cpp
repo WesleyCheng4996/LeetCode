5290ad83daceb0dea59ba01e215e7f86837e557f
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-11-03 23:25:43 +0800

    Time: 250 ms (44.26%), Space: 83.4 MB (47.79%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        vector<int>dollar(2, 0);
+        for(int x : bills) {
+            if(x == 5) {
+                ++dollar[0];
+            } else if(x == 10) {
+                if(dollar[0] == 0) {
+                    return false;
+                } else {
+                    --dollar[0];
+                    ++dollar[1];
+                }
+            } else {
+                if(dollar[1] == 0) {
+                    if(dollar[0] < 3) {
+                        return false;
+                    } else {
+                        dollar[0] -= 3;
+                    }
+                } else {
+                    if(dollar[0] == 0) {
+                        return false;
+                    } else {
+                        --dollar[0];
+                        --dollar[1];
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
