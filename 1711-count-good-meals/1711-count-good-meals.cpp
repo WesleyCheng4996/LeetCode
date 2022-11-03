7a8e7087b064df8c686a58d613879327ea7e7a52
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-11-03 18:48:57 +0800

    Time: 1390 ms (20.64%), Space: 336.4 MB (5.07%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -20,31 +20,29 @@ constexpr Table table;
 class Solution {
 public:
     int countPairs(vector<int>& deliciousness) {
-        unordered_map<int, long long>mp;
+        unordered_map<int, int>mp;
         vector<int>b(1 << 23, true);
-        double ret = 0;
+        long long ret = 0;
         for(int x : deliciousness) {
             ++mp[x];
         }
-        if(mp.size() > 1) {
         for(int x : deliciousness) {
-            for(int i = 0; i < 23; ++i) {
+            for(int i = 0; x + x >= table.arr[i]; ++i) {
                 if(x + x == table.arr[i]) {
                     if(b[x]) {
-                        ret += (table.arr2[mp[x] - 1] << 1);
+                        ret += table.arr2[mp[x] - 1];
+                        ret %= 1000000007;
                         b[x] = false;
                     } else {
                         continue;
                     }
                 } else if(mp.count(table.arr[i] - x)) {
-                    ret += (mp[table.arr[i] - x]);
+                    ret += mp[table.arr[i] - x];
+                    ret %= 1000000007;
                 }
             }
         }
-        return fmod(ret / 2, 1000000007.0);
-        } else {
-            return table.arr2[mp.begin()->second - 1]; 
-        }
+        return ret % 1000000007;
     }
 };
 
