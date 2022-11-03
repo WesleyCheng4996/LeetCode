8b8e89c48424cafbb2def510ee0c44716beb70e0
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-11-03 19:02:28 +0800

    Time: 433 ms (82.79%), Space: 58.2 MB (43.84%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -1,10 +1,16 @@
 struct Table {
     int power_2[31];
+    unsigned int arr2[100000];
     constexpr Table() :
-    power_2()
+    power_2(), arr2()
     {
         for(int i = 0; i < 31; ++i) {
             power_2[i] = (1 << i);
+        }
+        arr2[1] = 1;
+        for(int i = 2; i < 100000; ++i) {
+            arr2[i] = arr2[i - 1] + i;
+            arr2[i] %= 1000000007;
         }
     }
 };
@@ -14,18 +20,27 @@ constexpr Table table;
 class Solution {
 public:
     int countPairs(vector<int>& deliciousness) {
-        unordered_map<int, unsigned int>mp;
+        unordered_map<int, int>mp;
+        vector<int>b(33, true);
         unsigned int ret = 0;
         for(int x : deliciousness) {
-            for(int i = 0; i < 22; ++i) {
-                if(mp.count(table.power_2[i] - x)) {
-                    ret += mp[table.power_2[i] - x];
-                    ret %= 1000000007;
-                }
-            }
             ++mp[x];
         }
-        return ret;
+        for(int x : deliciousness) {
+            for(int i = 0; x + x >= table.power_2[i]; ++i) {
+                if(x + x == table.power_2[i]) {
+                    if(b[__builtin_clz(x)]) {
+                        ret += table.arr2[mp[x] - 1];
+                        b[__builtin_clz(x)] = false;
+                    } else {
+                        continue;
+                    }
+                } else if(mp.count(table.power_2[i] - x)) {
+                    ret += mp[table.power_2[i] - x];
+                }
+            }
+        }
+        return ret % 1000000007;
     }
 };
 
