0986e174653b8b0b03804affb16516d5879f23bc
WesleyCheng4996 <108218113+WesleyCheng4996@users.noreply.github.com>
2022-11-03 15:56:06 +0800

    Time: 479 ms (22.65%), Space: 94.2 MB (80.49%) - LeetHub

---
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        int max_num = nums[0], ret = 0;
+        for(int i = 1; i < nums.size(); ++i) {
+            if(nums[i] >= max_num) {
+                if(all_of(nums.begin() + i, nums.end(), [&](int num){ return num >= max_num; })) {
+                    return i;
+                } else {
+                    max_num = nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+};
